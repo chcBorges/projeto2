4e08092762a9070d8b2a1fec9a1d4d46d0b2fb99
carled7 <carlos_ed03@outlook.com>
2021-06-28 17:54:44 -0300

    Cadastrar banda criada
    A função rsponsável por cadastrar novas bandas foi criada.

--- versão1.1.c
+++ versão1.1.c
@@ -1,91 +1,136 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 int menu();
 
-typedef struct smusica{
-	char nome[30], compositor[30];
-	int nota;
-}MUSICA;
+typedef struct sBanda
+{
+    char nome[30], genero[30], anoCriacao[4];
+    int id;
+} BANDA;
+
+typedef struct smusica
+{
+    char nome[30], compositor[30];
+    int nota;
+} MUSICA;
 // Define a estrutura musica
 
-void cadastrarMusica(MUSICA musicas[], int i){
-	system("cls");
-    //Limpa tela do prompt de comando
-    
-	printf("\nDigite o nome da musica: ");
-	scanf("%s", musicas[i].nome);
-	// Le nome da musica
-	fflush(stdin);
-	printf("\nDigite o compositor da musica: ");
-	scanf("%s", musicas[i].compositor);
-	// Le compositor da musica
-	fflush(stdin);
-	musicas[i].nota = 0;
-	//Atribui 0 na nota
-	printf("Sua musica foi cadastrada com sucesso! Aperte qualquer tecla para voltar ao menu...");
-	getch();
-	// Esse comando eh para pausar a tela, quando apertar alguma coisa ele continua o codigo
+void cadastrarBanda(BANDA bandas[], int i)
+{
+    system("cls");      //Limpa tela do prompt de comando
+
+    printf("\nDigite o nome da banda: ");
+    scanf("%s", bandas[i].nome);    //realiza a leitura do nome da banda
+    fflush(stdin);
+
+    printf("\nDigite o genero da banda: ");
+    scanf("%s", bandas[i].genero);  //realiza a leitura do genero da banda
+    fflush(stdin);
+
+    printf("\nDigite o ano de criacao da banda: ");
+    scanf("%s", bandas[i].anoCriacao);  //realiza a leitura do ano de criacao da banda
+
+    bandas[i].id = i + 1;     //a possicao banda eh posta como id, assim nenhuma tera o mesmo id
+
+
+    system("cls");      //Limpa tela do prompt de comando
+
+    printf("\nBanda inserida com sucesso!");    //imprime que a banda foi inserida
+    sleep(2);   //espera dois segundos para retornar ao menu
+    menu();
 }
 
-int main(){
+void cadastrarMusica(MUSICA musicas[], int i)
+{
+    system("cls");
+    //Limpa tela do prompt de comando
+
+    printf("\nDigite o nome da musica: ");
+    scanf("%s", musicas[i].nome);
+    // Le nome da musica
+    fflush(stdin);
+    printf("\nDigite o compositor da musica: ");
+    scanf("%s", musicas[i].compositor);
+    // Le compositor da musica
+    fflush(stdin);
+    musicas[i].nota = 0;
+    //Atribui 0 na nota
+    printf("Sua musica foi cadastrada com sucesso! Aperte qualquer tecla para voltar ao menu...");
+    getch();
+    // Esse comando eh para pausar a tela, quando apertar alguma coisa ele continua o codigo
+}
+
+int main()
+{
     int opcao = menu();
-    //Recebe a opção digitada pelo usuário
-    
-    MUSICA musicas[10];
-    int posicaoMusica = 0;
-    // Define capacidade do sistema de armazenar as 10 musicas
-	// E define uma variavel para colocar as posicoes das musicas na funcao
-	
-    if(opcao == 0){
-    //Se a opção 0 for digitada encerra o programa
+    //Recebe a opï¿½ï¿½o digitada pelo usuï¿½rio
+
+    BANDA bandas[10];  //declara a espaco para 10 bandas na memoria
+    int bandaIndx = 0; //declara um indice que sera passado junto com a banda
+
+    MUSICA musicas[10];    // Define capacidade do sistema de armazenar as 10 musicas
+    int posicaoMusica = 0; // E define uma variavel para colocar as posicoes das musicas na funcao
+
+    if (opcao == 0)
+    {
+        //Se a opï¿½ï¿½o 0 for digitada encerra o programa
         char a = '\0';
         printf("Encerrando o programa... Pressione qualquer coisa para sair: ");
         fflush(stdin);
         scanf("%c", &a);
-        //Trava o programa até que o usuário digite uma coisa qualquer
-        return 1;
-        //Retorna 1 só pra diferenciar quando o programa termina aqui
+        //Trava o programa atï¿½ que o usuï¿½rio digite uma coisa qualquer
+        return 0;
+        //Retorna 1 sï¿½ pra diferenciar quando o programa termina aqui
         //de quando ele termina no fim do main
     }
-    else if(opcao == 1){
-    //Chama a função cadastra banda
+    else if (opcao == 1)
+    {
+        cadastrarBanda(bandas, bandaIndx);          //Chama a funcao cadastrar banda
+        bandaIndx++;                                //Incrementa 1 ao index das bandas 
     }
-    else if(opcao == 2){
-    //Chama a função cadastra musica
-    	cadastrarMusica(musicas, posicaoMusica);
-    	printf("%s", musicas[posicaoMusica].nome);
-    	posicaoMusica++;
-    	printf("\n**TESTE**  posicaoMusica = %d", posicaoMusica);
-    	getch();
-    	// esta parte nao esta pronta ainda, eh so pra eu observar o comportamento 
+    else if (opcao == 2)
+    {
+        cadastrarMusica(musicas, posicaoMusica);    //Chama a funcao cadastra musica
+        printf("%s", musicas[posicaoMusica].nome);
+        posicaoMusica++;
+        printf("\n**TESTE**  posicaoMusica = %d", posicaoMusica);
+        getch();
+        // esta parte nao esta pronta ainda, eh so pra eu observar o comportamento
     }
-    else if(opcao == 3){
-    //Chama a função de imprimir todas as bandas cadastradas
+    else if (opcao == 3)
+    {
+        //Chama a funï¿½ï¿½o de imprimir todas as bandas cadastradas
     }
-    else if(opcao == 4){
-    //Chama a função de imprimir a musica com a maior nota
+    else if (opcao == 4)
+    {
+        //Chama a funï¿½ï¿½o de imprimir a musica com a maior nota
     }
-    else if(opcao == 5){
-    //Chama a função de imprimir a musica com a menor nota
+    else if (opcao == 5)
+    {
+        //Chama a funï¿½ï¿½o de imprimir a musica com a menor nota
     }
-    else if(opcao == 6){
-    //Chama a função de buscar a banda pelo genero
+    else if (opcao == 6)
+    {
+        //Chama a funï¿½ï¿½o de buscar a banda pelo genero
     }
-    else if(opcao == 7){
-    //Chama a função de avaliar musica
+    else if (opcao == 7)
+    {
+        //Chama a funï¿½ï¿½o de avaliar musica
     }
 
     return 0;
 }
 
-int menu(){
+int menu()
+{
     int opcao = 0;
-    //Variável que vai receber o que o usuário digitar
+    //Variï¿½vel que vai receber o que o usuï¿½rio digitar
     //E posteriormente vai ser retornada
     char traco[15] = {205, 205, 205, 205, 205, 205, 205, 205, 205, 205};
-    //Uma string para fins de estetica do programa. São 10 caracteres parecidos com o =
+    //Uma string para fins de estetica do programa. Sï¿½o 10 caracteres parecidos com o =
 
     system("cls");
     //Limpa tela do prompt de comando
@@ -98,20 +143,19 @@ int menu(){
     printf("\n5 - Musica com menor nota");
     printf("\n6 - Buscar banda pelo genero");
     printf("\n7 - Avaliar uma musica");
-    //Apresenta as opções disponivéis
+    //Apresenta as opï¿½ï¿½es disponivï¿½is
 
     printf("\n\nDigite uma opcao valida: ");
     scanf("%d", &opcao);
-    //Recebe a opção do usuário
+    //Recebe a opï¿½ï¿½o do usuï¿½rio
 
-    if((opcao < 0) || (opcao > 7)){
-    //Verifica se a opção digitado pelo usuário está de fora do intervalo permitido
-    //Se estiver chama a função menu novamente
+    if ((opcao < 0) || (opcao > 7))
+    {
+        //Verifica se a opï¿½ï¿½o digitado pelo usuï¿½rio estï¿½ de fora do intervalo permitido
+        //Se estiver chama a funï¿½ï¿½o menu novamente
         menu();
     }
 
     return opcao;
-    //Retorna a opção válida que foi digitada pelo usuário
+    //Retorna a opï¿½ï¿½o vï¿½lida que foi digitada pelo usuï¿½rio
 }
-
-
